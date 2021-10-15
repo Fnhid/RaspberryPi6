d90580cdfdc1355e18cdea0e3c19cab41ab5c93c
Fnhid <fnhid05@gmail.com>
2021-10-15 16:36:39 +0900

    add some codes

--- 06_multimedia/opencv_camera_out.py
+++ 06_multimedia/opencv_camera_out.py
@@ -6,6 +6,12 @@ if not cap.isOpened():
     print('Camera open failed')
     exit()
 
+# fourcc(four character code)
+# DIVX(avi), MP4V(mp4), X264(h264)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX') # ('D', 'I', 'V', 'X')
+
+out = cv2.VideoWriter('output.avi', fourcc, 30, (640, 480))
+
 # 카메라 사진 찍기
 #ret, frame = cap.read()
 #cv2.imshow('frame', frame)
@@ -19,6 +25,8 @@ while True:
         break
 
     cv2.imshow('frame', frame)
+    out.write(frame)
+
     # 1000->1초, 10->0.01초
     if cv2.waitKey(10) == 13:
         break
@@ -30,4 +38,5 @@ while True:
 
 # 사용자 지원 해제
 cap.release()
+out.release()
 cv2.destroyAllWindows()
